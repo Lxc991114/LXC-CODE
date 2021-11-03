e661df9b2e163f0199cdbe9f320c283186278095
Lxc991114 <253248448@qq.com>
2021-11-03 18:42:47 +0800

    JavaScript基础内容

---
+++ code-lxc/3_JavaScript/2-2.外部js.js
@@ -0,0 +1,3 @@
+document.getElementById('btn2').onclick = function(){
+    console.log('这是外部js的按钮');
+}
---
+++ code-lxc/3_JavaScript/3-1.文档注释.js
@@ -0,0 +1,8 @@
+/**
+ * @param name 这是姓名
+ * @param age 这是年龄
+ * @returns 一个字符串
+ */
+ function test(name, age) {
+     return 'xxx';
+ }
